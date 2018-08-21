bb64feaa005f90f27cfd9237ae95aa6f307f3015
wlalzptxm <wlalzptx11@gmail.comm>
2018-08-21 15:25:47 +0900

    計算機の基本骨格

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,20 +6,40 @@
 //  Copyright © 2018 Seunghoon Jeon. All rights reserved.
 //
 
+//UIKitにはButton、TextFieldのような基本的なすべてのUIが入っている
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //optionalはnotsetの状態でいつも初期化される
+    @IBOutlet weak var display: UILabel!
+    
+    // 一般propertyは、初期化が必要してswiftはタイプを推論するために:Boolは省略可能である
+    var userIsInTheMiddleOfTyping = false
+    
+    //アンダーバーを使用してexternal名前を省略することができる
+    @IBAction func touchDigit(_ sender: UIButton) {
+        //letを使用するとき二つの理由がある。
+        //1. コードを読む人たちに絶対に変わるはずないと明示してくれる
+        //2. ArrayやDictionaryにletを使用するなら、どの値も追加で入ったり、取り出したりすることができないということを意味する(読みだけに可能なものとつくる方法の一つ)
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrnetlyInDisplay = display.text!
+            display.text = textCurrnetlyInDisplay + digit
+        }else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func performOpertation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                //displayはdoubleタイプが受けられないため、Stringでcasting
+                display.text = String(Double.pi)
+            }
+        }
     }
-
-
 }
 
